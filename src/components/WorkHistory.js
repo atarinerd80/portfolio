456a4cc66bce069c93509429fffaa2fa543325b4
Thomas Slagle <thomas.slagle@slaglesoft.com>
2022-05-11 14:43:04 -0400

    Added portfolio item

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -3,7 +3,7 @@ import React from "react";
 export class WorkHistory extends React.Component {
     render() {
         const sectionClass = (this.props.index % 2 === 0 ? 'timeline-item mt-5 mt-lg-2' : 'timeline-item timeline-inverted mt-5 mt-lg-2');
-
+        const triangleClass = (this.props.index % 2 === 0) ? 'timeline-content timeline-content-first p-3 p-lg-4 triangle-yellow' : 'timeline-content timeline-content-second p-3 p-lg-4 triangle-yellow'
         return (
             <>
                 <div className={sectionClass}>
@@ -11,7 +11,7 @@ export class WorkHistory extends React.Component {
                         <span className="bg-pink font-alt font-w-700 letter-spacing-1 shadow text-small text-uppercase text-white">{this.props.job.years}</span>
                     </div>
                     <i className="bg-white far fa-dot-circle fa-2x text-purple"></i>
-                    <div className="timeline-content p-3 p-lg-4 triangle-yellow">
+                    <div className={triangleClass}>
                         <span className="font-w-700 letter-spacing-1 text-small text-uppercase">{this.props.job.title}</span> 
                         <h4 className="font-alt letter-spacing-1 mb-0 text-medium text-uppercase">{this.props.job.company}</h4>
                         <div className="bg-gray-100 mt-3 p-3 rounded">
